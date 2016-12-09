aadc2e995cf73c4e52092d8bf6d15bbaf4b4487d
ChristophTek <ChristophTek@users.noreply.github.com>
2016-12-09 14:16:35 +0000

    Update views.py
    Partly commented code

--- code/Register/upload/views.py
+++ code/Register/upload/views.py
@@ -8,12 +8,12 @@ from django.core.urlresolvers import reverse
 
 from upload.models import Document
 from upload.forms import docform
-
-def upload(request):
+                
+def upload(request):                                                     #This part of the code controls the upload function
     if request.method == 'POST':
-        form = docform(request.POST, request.FILES)
-        if form.is_valid():
-            dan = Document (newfile=request.FILES['newfile'])
+        form = docform(request.POST, request.FILES)                     #Creates a POST method for the upload system 
+        if form.is_valid():                                             #If the form is valid proceed with the upload
+            dan = Document (newfile=request.FILES['newfile %y %m d%'])  # This takes the file add Newfile to the tag with the year, month, and day     
             dan.save()
         return HttpResponseRedirect(reverse('upload'))
     else:
