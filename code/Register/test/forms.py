b8bccdbae2deb502d56416f44e1ec939aaae35ec
CarlFitz93 <carlfitzgerald1993@gmail.com>
2016-12-13 19:01:16 +0000

    Update forms.py

--- code/Register/test/forms.py
+++ code/Register/test/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 
 
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+class UserForm(forms.ModelForm): #Creating a new class called UserForm to be called upon in views.py, I have included forms.ModelForm as the form is going to be mapped directly to the user model
+    password = forms.CharField(widget=forms.PasswordInput) #Creates string field for Password, the widget argument tells Dango how to handle the HTML input. The widget deals with data from POST/GETs 
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
+    class Meta: #This is a class sets Metadeta to the form 
+        model = User #Associate our user model directly to the form
+        fields = ['username', 'email', 'password'] #Express which fields can be written in by the user on the html, otherwise user could set admin rights to themselves.
